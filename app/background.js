aadf1876bafc58d7452ece284d203f8dbfa1b659
Samuel Dobbie <samuel.jordan.dobbie@gmail.com>
2020-06-28 21:57:15 +0100

    Construct and send query to analysis api

--- app/background.js
+++ app/background.js
@@ -1,3 +1,17 @@
+var apiUrl = 'http://127.0.0.1:8000';
+
 chrome.browserAction.onClicked.addListener(function(activeTab) {
-    chrome.tabs.executeScript(null, {file: "content.js"});
+    // Get id and url of active tab
+    var tabId = activeTab.id;
+    var tabUrl = activeTab.url;
+    
+    // Construct url for api query
+    var queryUrl = apiUrl + '/query/' + tabUrl + '/';
+    
+	// Send API query
+    var request = new XMLHttpRequest();
+    request.open('GET', queryUrl, true);
+    request.send();
+	
+	// To-do: Validate that the url is a valid news article
 });
