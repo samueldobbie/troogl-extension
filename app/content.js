1d33d62d90ff5f8bf9f6e15067e72779d874abdb
Samuel Dobbie <samuel.jordan.dobbie@gmail.com>
2020-06-29 02:18:38 +0100

    Add class to each sentence within article

--- app/content.js
+++ app/content.js
@@ -6,6 +6,24 @@ var headline = response['headline'];
 var body = response['body'];
 var sentences = response['sentences'];
 
-console.log(headline);
-console.log(body);
-console.log(sentences);
+// Get scroll positioning within page
+var pageXOffset = window.pageXOffset;
+var pageYOffset = window.pageYOffset;
+
+// Enable editing of article
+document.designMode = 'on';
+
+for (var i = 0; i < sentences.length; i++) {
+    // Find sentence within page
+    window.find(sentences[i]);
+
+    // Add class to each sentence
+    var sentenceElement = window.getSelection().baseNode.parentElement;
+    sentenceElement.classList.add('troogl-sentence');
+}
+
+// Disable editing of article
+document.designMode = 'off';
+
+// Return page scroll to its original position
+window.scrollTo(pageXOffset, pageYOffset);
