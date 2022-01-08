37f280ece1e7023ca905ce9f9d8f9643bc5112fb
samuel <samuel.jordan.dobbie@gmail.com>
2022-01-08 02:08:09 +0000

    Add material-ui + basic placeholder popup

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,12 +1,55 @@
 import React from "react"
 import { render } from "react-dom"
+import { Button, Paper, TextField, Typography } from "@mui/material"
 
 function Popup(): JSX.Element {
   return (
-    <div>
-      Hello, World!
-    </div>
+    <Paper
+      sx={{
+        height: "100%",
+        textAlign: "center",
+        padding: 2,
+      }}
+    >
+      <Typography
+        gutterBottom
+        sx={{
+          fontSize: 10,
+          textAlign: "left",
+        }}
+      >
+        Troogl will automatically analyze any page
+        with a URL matching one of the wildcards below.
+      </Typography>
+
+      <TextField
+        multiline
+        fullWidth
+        rows={10}
+        label="Update Wildcards"
+        placeholder="https://www.bbc.co.uk/news/*"
+        variant="filled"
+        sx={{ marginBottom: 2 }}
+      />
+
+      <Typography sx={{ marginBottom: 2 }}>
+        --- OR ---
+      </Typography>
+
+      <Button
+        fullWidth
+        onClick={analyzePage}
+        variant="contained"
+      >
+        Analyze open page
+      </Button>
+    </Paper>
   )
+}
+
+const analyzePage = () => {
+  console.log(1)
+  alert(2)
 }
 
 render(<Popup />, document.getElementById("root"))
