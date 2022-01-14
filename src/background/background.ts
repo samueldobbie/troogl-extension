697fcd28a5133d60e174af1159f7502f55d169a6
samuel <samuel.jordan.dobbie@gmail.com>
2022-01-14 22:50:50 +0000

    Extract sentences from page

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,8 +1,26 @@
-const handleActionClick = () => {
-  console.log(1)
-  alert(2)
+import TabChangeInfo = chrome.tabs.TabChangeInfo
+import Tab = chrome.tabs.Tab
+
+const wildCards = [ "https://www.bbc.co.uk/news/*" ]
+
+const handleInstall = () => {
+  chrome.tabs.create({ url: "welcome.html" })
 }
 
-chrome.browserAction.onClicked.addListener(handleActionClick)
+const handleUpdated = (tabId: number, changeInfo: TabChangeInfo, tab: Tab) => {
+  const tabUrl = tab.url
 
-export {}
+  if (changeInfo.status === "complete" && tabUrl) {
+    const hasMatch = wildCards.some(wildCard => tabUrl.match(wildCard))
+
+    if (hasMatch) {
+      chrome.tabs.sendMessage(tabId, {
+        topic: "TabUpdated",
+        payload: { url: tabUrl },
+      })
+    }
+  }
+}
+
+chrome.runtime.onInstalled.addListener(handleInstall)
+chrome.tabs.onUpdated.addListener(handleUpdated)
