bab9f8883ac772da3293036758c0bf1abbad610a
samuel <samuel.jordan.dobbie@gmail.com>
2022-01-16 16:17:44 +0000

    Add container for full-screen dashboard

--- src/content/components/dashboard.tsx
+++ src/content/components/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, ToggleButton, ToggleButtonGroup } from "@mui/material"
+import { Button, Container, Grid, IconButton, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material"
 import React, { useState } from "react"
 import DragIndicatorIcon from "@mui/icons-material/DragIndicator"
 import Draggable from "react-draggable"
@@ -6,6 +6,7 @@ import { ISentence } from "../commons/interfaces/sentence"
 import LineChart from "./line-chart"
 import { ThemeProvider } from "@emotion/react"
 import { theme } from "../commons/configs/theme"
+import CloseIcon from "@mui/icons-material/Close"
 
 interface IProps {
   sentences: ISentence[]
@@ -14,33 +15,44 @@ interface IProps {
 function Dashboard(props: IProps): JSX.Element {
   const { sentences } = props
 
-  const [hide, setHide] = useState(false)
+  const [mode, setMode] = useState("partial")
 
   return (
     <ThemeProvider theme={theme}>
-      {
-        hide
-          ? <CollapsedDashboard setHide={setHide} />
-          : <FullDashboard
-              sentences={sentences}
-              setHide={setHide}
-            />
+      {mode === "hidden" &&
+        <CollapsedDashboard
+          setMode={setMode}
+        />
+      }
+
+      {mode === "partial" &&
+        <PartialDashboard
+          sentences={sentences}
+          setMode={setMode}
+        />
+      }
+
+      {mode === "full" &&
+        <FullDashboard
+          sentences={sentences}
+          setMode={setMode}
+        />
       }
     </ThemeProvider>
   )
 }
 
 interface ICollapsedProps {
-  setHide: (hide: boolean) => void
+  setMode: (mode: string) => void
 }
 
 function CollapsedDashboard(props: ICollapsedProps): JSX.Element {
-  const { setHide } = props
+  const { setMode } = props
 
   return (
     <Button
       variant="contained"
-      onClick={() => setHide(false)}
+      onClick={() => setMode("partial")}
       sx={{
         position: "fixed",
         bottom: 0,
@@ -56,18 +68,20 @@ function CollapsedDashboard(props: ICollapsedProps): JSX.Element {
   )
 }
 
-interface IFullDashboardProps {
+interface IPartialDashboardProps {
   sentences: ISentence[]
-  setHide: (value: boolean) => void
+  setMode: (mode: string) => void
 }
 
-function FullDashboard(props: IFullDashboardProps): JSX.Element {
-  const { sentences, setHide } = props
+function PartialDashboard(props: IPartialDashboardProps): JSX.Element {
+  const { sentences, setMode } = props
 
   const [metricType, setMetricType] = useState("entity")
 
-  const toggleMetricType = (type: string) => {
-    setMetricType(type)
+  const toggleMetricType = (type: string | null) => {
+    if (type) {
+      setMetricType(type)
+    }
   }
 
   return (
@@ -133,6 +147,7 @@ function FullDashboard(props: IFullDashboardProps): JSX.Element {
             <Button
               variant="contained"
               color="secondary"
+              onClick={(): void => setMode("full")}
               sx={{ marginRight: 1 }}
             >
               Expand
@@ -141,13 +156,87 @@ function FullDashboard(props: IFullDashboardProps): JSX.Element {
             <Button
               variant="contained"
               color="secondary"
-              onClick={(): void => setHide(true)}
+              onClick={(): void => setMode("hidden")}
             >
               Hide
             </Button>
           </Grid>
         </Grid>
       </Draggable>
+    </div>
+  )
+}
+
+interface IFullDashboardProps {
+  sentences: ISentence[]
+  setMode: (mode: string) => void
+}
+
+function FullDashboard(props: IFullDashboardProps): JSX.Element {
+  const { sentences, setMode } = props
+
+  const [metricType, setMetricType] = useState("entity")
+  
+  const text = sentences.map(sentence => sentence.text).join(" ")
+  const summary = sentences.slice(0, 200)
+
+  const toggleMetricType = (type: string | null) => {
+    if (type) {
+      setMetricType(type)
+    }
+  }
+
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        height: "100vh",
+        width: "100vw",
+        zIndex: 2147483647,
+        backgroundColor: "#eceded",
+      }}
+    >
+      <IconButton
+        color="primary"
+        onClick={() => setMode("partial")}
+        sx={{
+          position: "absolute",
+          top: "2.5%",
+          right: "2.5%",
+        }}
+      >
+        <CloseIcon />
+      </IconButton>
+
+      <Grid
+        container
+        spacing={0}
+        alignItems="center"
+        sx={{
+          marginTop: "5%",
+          textAlign: "center",
+        }}
+      >
+        <Grid item xs={12}>
+          <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+            Summary
+          </Typography>
+
+          <Typography variant="body1" sx={{ fontWeight: "bold" }}>
+            {summary}
+          </Typography>
+        </Grid>
+
+        <Grid item xs={3}>
+          MORE DETAILS
+        </Grid>
+
+        <Grid item xs={9}>
+          SOME LONGER DETAILS
+        </Grid>
+      </Grid>
     </div>
   )
 }
